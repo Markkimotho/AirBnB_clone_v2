0df6815ffebe918c1510a075f928c3ee8c0bb032
Markkimotho <martinozas@gmail.com>
2023-05-08 23:52:02 +0300

    101-setup_web_static.pp.1

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,68 +1,40 @@
 # puppet file that sets up my web servers for the deployment of web_static
+# Install Nginx if it's not already installed
 package { 'nginx':
   ensure => installed,
 }
 
-file { '/data':
+# Start the Nginx service
+service { 'nginx':
+  ensure => running,
+  enable => true,
+}
+
+# Create the necessary directories
+file { ['/data', '/data/web_static', '/data/web_static/releases', '/data/web_static/shared', '/data/web_static/releases/test']:
   ensure => directory,
   owner  => 'ubuntu',
   group  => 'ubuntu',
 }
 
-file { '/data/web_static':
-  ensure => directory,
-  owner  => 'ubuntu',
-  group  => 'ubuntu',
-}
-
-file { '/data/web_static/releases':
-  ensure => directory,
-  owner  => 'ubuntu',
-  group  => 'ubuntu',
-}
-
-file { '/data/web_static/shared':
-  ensure => directory,
-  owner  => 'ubuntu',
-  group  => 'ubuntu',
-}
-
+# Create the test HTML file
 file { '/data/web_static/releases/test/index.html':
   content => 'Holberton School',
   owner   => 'ubuntu',
   group   => 'ubuntu',
 }
 
+# Create the symbolic link
 file { '/data/web_static/current':
-  ensure => link,
-  target => '/data/web_static/releases/test/',
-  owner  => 'ubuntu',
-  group  => 'ubuntu',
-}
-
-file { '/etc/nginx/sites-available/default':
-  ensure => present,
-  source => 'puppet:///modules/mymodule/default',
-}
-
-file { '/etc/nginx/sites-enabled/default':
   ensure => 'link',
-  target => '/etc/nginx/sites-available/default',
+  target => '/data/web_static/releases/test',
 }
 
+# Update the Nginx configuration
 file { '/etc/nginx/sites-available/default':
-  content => template('mymodule/nginx.conf.erb'),
-}
-
-service { 'nginx':
-  ensure  => running,
-  enable  => true,
-  require => [
-    File['/data/web_static/current'],
-    File['/etc/nginx/sites-enabled/default'],
-  ],
-  subscribe => [
-    File['/etc/nginx/sites-available/default'],
-    File['/data/web_static/current'],
-  ],
+  content => template('nginx/default.erb'),
+  owner   => 'root',
+  group   => 'root',
+  mode    => '0644',
+  notify  => Service['nginx'],
 }
