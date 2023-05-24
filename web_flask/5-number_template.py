24ba02fa9aee8a74e480a69cc892c80b4be1ebe6
MarkTinega <martinozas@gmail.com>
2023-05-24 14:18:42 +0300

    Added all the routes

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""script that starts a Flask web application
+"""
+
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Returns greeting
+    """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Return hbnb
+    """
+    return 'HBNB'
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """Return C_text
+    """
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text="is cool"):
+    """Return Python Text
+    """
+    return 'Python {}'.format(text.replace('_',' '))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def is_number(n):
+    """Returns whether number or not
+    """
+    return '{} is a number'.format(n)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def cast_number(n):
+    """Returns an html docstring with the number cast onto it
+    """
+    path = '5-number.html'
+    return render_template(path, n=n)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
